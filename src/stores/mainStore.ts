97ff04b22d315da17fc5df8164b583b5de8e3156
smithst <62660737+smithst18@users.noreply.github.com>
2025-01-08 11:45:47 -0400

    feat: deploy update

--- src/stores/mainStore.ts
+++ src/stores/mainStore.ts
@@ -31,31 +31,36 @@ export const useMainStore = defineStore('main', () => {
 
   // =======================================> ACTIONS
   const logIn = async ( form : object ) => {
+    try{
 
-    const data = await logInService(form);
-    if(data.token) {
-      //save the token in the cookies for the axios interceptor
-      cookies.set('token', data.token, 60*60*12);
-      //send the request again with the token in the header
-      const payload = await logInService(form);
-      // we get the payload of the user information and save it in the cookies for use it lately
-      cookies.set('user', payload.user, 60*60*12);
-      //redirect to home view
-      logedUser.value = payload.user;
-      isLoged.value = true;
-      return '200'
-    }
-    //goint this bloq if user error
-    else if(data.response) {
-      //set spinner in loggin vue  to false
-      requestIsLoading.value = false;
-      return '400';
-    }
-    //goint this bloq if server error
-    else {
-      //set spinner in loggin vue to false
-      requestIsLoading.value = false;
-      return "500";
+      const data = await logInService(form);
+      if(data.token) {
+        //save the token in the cookies for the axios interceptor
+        cookies.set('token', data.token, 60*60*12);
+        //send the request again with the token in the header
+        const payload = await logInService(form);
+        // we get the payload of the user information and save it in the cookies for use it lately
+        cookies.set('user', payload.user, 60*60*12);
+        //redirect to home view
+        logedUser.value = payload.user;
+        isLoged.value = true;
+        return '200'
+      }
+      //goint this bloq if user error
+      else if(data.response) {
+        //set spinner in loggin vue  to false
+        requestIsLoading.value = false;
+        return '400';
+      }
+      //goint this bloq if server error
+      else {
+        console.error(data)
+        //set spinner in loggin vue to false
+        requestIsLoading.value = false;
+        return "500";
+      }
+    }catch(err){
+      console.log(err);
     }
   }
 
